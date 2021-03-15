aac7df08f27b259679f0bba19028ed39f3048f86
JeremieC <certain.j@live.fr>
2021-03-15 19:18:59 +0100

    socketio tutorial - users connection with counter

---
+++ app.js
@@ -0,0 +1,99 @@
+//Modules
+const express = require('express')
+const morgan = require('morgan')
+const config = require('./config.json')
+const http = require('http')
+const socketio = require('socket.io')
+
+
+//Globals
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+const port = config.express.port
+const options = {
+    root: __dirname + '/views/'
+}
+
+let usernames = []
+
+//Midllewares
+/*app.use(function (req, res, next) {
+    console.log('Time: %d', Date.now())
+    console.log(req.path)
+    next()
+})*/
+app.use(express.static(options.root));
+app.use(morgan('dev'));
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.redirect('/home')
+})
+app.get('/home', (req, res) => {
+    res.sendFile('index.html', options)
+})
+
+app.get('/params/:name', (req, res) => {
+    res.send('URL ' + req.params.name)
+})
+
+
+//IO
+io.on('connection', (socket) => {
+    console.log('IO connection is now established by user ' + socket.id)
+
+    //Set username event
+    socket.on('setUsername', (usernameWanted) => {
+        usernameWanted = usernameWanted.trim()
+
+        //Check if username is unique
+        let usernameTaken = false
+        for(let socketid in usernames) {
+            if(usernames[socketid] == usernameWanted) {
+                usernameTaken = true
+            }
+        }
+
+        let timeFakeLoading = 0
+        setTimeout(() => {
+            //Actions
+            if(usernameTaken) {
+                socket.emit('rejectUsername', usernameWanted)
+            } else {
+                socket.join('users')
+                usernames[socket.id] = usernameWanted
+                let justUsernames = getUsernames()
+                socket.emit('acceptUsername', usernameWanted, justUsernames)
+                socket.to('users').emit('newUserConnected', usernameWanted, justUsernames)
+            }
+        }, timeFakeLoading)
+
+    })
+
+    //Log out event
+    socket.on('disconnect', () => {
+        console.log('disconnection from user ' + socket.id)
+        if(usernames[socket.id]) {
+            let oldUsername = usernames[socket.id]
+            delete usernames[socket.id]
+            socket.to('users').emit('leftSessionUser', getUsernames(), oldUsername)
+        }
+    })
+})
+
+//Starts the App
+server.listen(port, () => {
+    console.log('Connected to App ! On Port: ' + port)
+})
+
+
+//Send array with usernames without indexes
+function getUsernames() {
+    let users = []
+    for(let socketid in usernames) {
+        users.push(usernames[socketid])
+    }
+    return users
+}
---
+++ views/assets/js/index.js
@@ -0,0 +1,98 @@
+document.querySelector('.chat[data-chat=person0]').classList.add('active-chat');
+document.querySelector('.person[data-chat=person0]').classList.add('active');
+
+let friends = {
+  list: document.querySelector('ul.people'),
+  all: document.querySelectorAll('.left .person'),
+  name: '' },
+
+chat = {
+  container: document.querySelector('.container .right'),
+  current: null,
+  person: null,
+  name: document.querySelector('.container .right .top .name') };
+
+
+friends.all.forEach(function (f) {
+  f.addEventListener('mousedown', function () {
+    f.classList.contains('active') || setAciveChat(f);
+  });
+});
+
+checkLinkUsersModal()
+
+function setAciveChat(f) {
+  friends.list.querySelector('.active').classList.remove('active');
+  f.classList.add('active');
+  chat.current = chat.container.querySelector('.active-chat');
+  chat.person = f.getAttribute('data-chat');
+  chat.current.classList.remove('active-chat');
+  chat.container.querySelector('[data-chat="' + chat.person + '"]').classList.add('active-chat');
+  friends.name = f.querySelector('.name').innerText;
+  chat.name.innerHTML = friends.name;
+  checkLinkUsersModal()
+}
+
+// Vérification de l'usabilité du lien ver le modal users
+function checkLinkUsersModal() {
+  if (chat.person == 'person0' || chat.person == null) {
+    chat.name.addEventListener('click', openUsersModal, false)
+    chat.name.style.cursor = 'pointer'
+  } else {
+    chat.name.removeEventListener('click', openUsersModal, false)
+    chat.name.style.cursor = 'default'
+  }
+}
+
+/* Modal */
+
+let modalContainer = document.body.querySelector('#modal-container'),
+    listUsers = modalContainer.querySelector('#contentUsers ul')
+
+// Ouverture du modal sur la demande de l'username
+function openUsernameModal() {
+  modalContainer.querySelector('#contentUsername').classList.remove('none')
+  modalContainer.querySelector('#contentUsers').classList.add('none')
+  modalContainer.classList.remove('out')
+  document.body.classList.add('modal-active')
+}
+
+// Ouverture du modal sur la liste des utilisateurs connectés
+function openUsersModal() {
+  modalContainer.querySelector('#contentUsername').classList.add('none')
+  modalContainer.querySelector('#contentUsers').classList.remove('none')
+  modalContainer.classList.remove('out')
+  document.body.classList.add('modal-active')
+}
+
+// Fermeture du modal
+function closeModal() {
+  modalContainer.classList.add('out')
+  document.body.classList.remove('modal-active')
+}
+
+
+function updateUsers(users) {
+  listUsers.innerHTML = ''
+  for(let i in users) {
+    listUsers.innerHTML += '<li>' + users[i] + '</li>'
+  }
+
+  let text = 'General Room(' + users.length + ')'
+
+  friends.all[0].querySelector('.name').innerHTML = text
+
+  if(chat.person == 'person0' || chat.person == null) {
+    document.body.querySelector('#infoPersonTop').innerHTML = text
+  }
+}
+
+function messageNewUserConnected(newUsername) {
+  let message = '<div class="conversation-start"><span>' + newUsername + ' is online !</span></div>'
+  chat.container.querySelector('.chat[data-chat=person0]').innerHTML += message
+}
+
+function messageleftSessionUser(newUsername) {
+  let message = '<div class="conversation-start"><span>' + newUsername + ' has left !</span></div>'
+  chat.container.querySelector('.chat[data-chat=person0]').innerHTML += message
+}
